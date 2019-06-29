09bf96cf0a98ff22f0655c7c1327400c292f8873
rajadeja <raj.jdja@gmail.com>
2019-06-29 17:14:50 +0000

    whyred: adapt for ArrowOS

--- arrow_whyred.mk
+++ arrow_whyred.mk
@@ -25,9 +25,9 @@ $(call inherit-product, device/xiaomi/whyred/device.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
-PRODUCT_NAME := potato_whyred
+PRODUCT_NAME := arrow_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
