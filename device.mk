38622940a6d56dffd7e27aef2f3904127dd370b1
srfarias <erikofarias@gmail.com>
2019-07-05 22:01:51 -0300

    whyred: update audio features and props from LA.UM.7.2.r1-07000-sdm660.0
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- device.mk
+++ device.mk
@@ -93,7 +93,12 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl:32 \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl:32 \
+    android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl:32 \
+    android.hardware.soundtrigger@2.1-service \
+    android.hardware.audio@4.0 \
+    android.hardware.audio.common@4.0 \
+    android.hardware.audio.common@4.0-util \
     audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
