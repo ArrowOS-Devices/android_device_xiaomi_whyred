ccc842df799c7d9d130c1a1c3924b13b1c90f8d8
srfarias <erikofarias@gmail.com>
2019-07-15 18:24:23 -0300

    whyred: declare omx.media hild on mk
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- device.mk
+++ device.mk
@@ -312,7 +312,9 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
     libc2dcolorconvert \
+    libextmedia_jni \
     libmm-omxcore \
     libOmxAacEnc \
     libOmxAmrEnc \
