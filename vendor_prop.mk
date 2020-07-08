c586885b477ac883b6ceac51497a14af9c1ad596
sreekfreak995 <sreekfreak995@gmail.com>
2020-07-08 15:33:52 +0530

    Revert "whyred: Add persist.radio.aosp_usr_pref_sel for Telephony"
    This reverts commit 91e899829b3e6e98c59146e3919bc92c908479ea.
    
    * It's already there yo

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -108,10 +108,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.media.codec2=2
 
-# Make data selection stick in dual SIM devices
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.radio.aosp_usr_pref_sel=true
-
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
