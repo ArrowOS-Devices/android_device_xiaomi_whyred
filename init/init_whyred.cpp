e841d1c0bb11405935bba99ac969cbea4dd7de6b
Lucchetto <lucchetto.tie@live.com>
2020-09-18 15:06:09 +0200

    whyred: adapt libinit to Android 11

--- init/init_whyred.cpp
+++ init/init_whyred.cpp
@@ -37,7 +37,10 @@
 #include "vendor_init.h"
 
 using android::base::GetProperty;
-using android::init::property_set;
+
+int property_set(const char *key, const char *value) {
+    return __system_property_set(key, value);
+}
 
 void property_override(char const prop[], char const value[])
 {
