21ef423790b25bf9fac8e87d8823ec57b8879ae5
Palakurthi Sreekanth <sreekfreak995@gmail.com>
2020-10-06 20:13:30 +0530

    whyred: enable A11 live blur by default
    * Also add props to make it smoother on our little monster

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -191,3 +191,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.bluetooth.soc=cherokee
+    
+#BlurGang 
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
